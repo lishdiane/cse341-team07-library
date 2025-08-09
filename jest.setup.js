eaf2a6add6555dfeb4a50888c36fba45cf53d0ee
lishdiane <dlish@byupathway.edu>
2025-08-09 13:05:07 -0600

    uncommented jest.setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,8 @@
-// jest.mock('passport', () => ({
-//   use: jest.fn(),
-//   authenticate: () => (req, res, next) => next(),
-//   initialize: () => (req, res, next) => next(),
-//   session: () => (req, res, next) => next(),
-//   serializeUser: jest.fn(),
-//   deserializeUser: jest.fn(),
-// }));
+jest.mock('passport', () => ({
+  use: jest.fn(),
+  authenticate: () => (req, res, next) => next(),
+  initialize: () => (req, res, next) => next(),
+  session: () => (req, res, next) => next(),
+  serializeUser: jest.fn(),
+  deserializeUser: jest.fn(),
+}));
