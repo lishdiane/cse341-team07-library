4b454e40ad72eac0f1517f610ea4ce5ff74ab956
TeAraErueti <keitepai1@gmail.com>
2025-08-09 20:02:39 +1200

    added unit testing that works locally under npm run test:jest

--- server.js
+++ server.js
@@ -4,69 +4,64 @@ const app = express();
 const bodyParser = require("body-parser");
 const inventoryRoute = require("./routes/bookRoute");
 const accountRoute = require("./routes/accountRoute");
-// new routes
-const magazineRoute = require("./routes/magazineRoute"); 
-const digitalRoute = require("./routes/digitalRoute");   
+const magazineRoute = require("./routes/magazineRoute");
+const digitalRoute = require("./routes/digitalRoute");
 const route = require("./routes/index");
-const PORT = process.env.PORT;
 const passport = require("passport");
 const session = require("express-session");
 const GithubStrategy = require("passport-github2").Strategy;
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
-
 /* ***********************
  * Middleware
- * ************************/
+ * ***********************/
 
-app
-  .use(
-    session({
-      secret: "secret",
-      resave: false,
-      saveUninitialized: true,
-    })
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json());
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
   )
-  .use(passport.initialize())
-  .use(passport.session())
-  .use(bodyParser.json());
+);
 
-  passport.use(
-    new GithubStrategy(
-      {
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: process.env.CALLBACK_URL,
-      },
-      (accessToken, refreshToken, profile, done) => {
-        return done(null, profile);
-      }
-    )
-  );
-  
-  passport.serializeUser((user, done) => {
-    done(null, user);
-  });
-  passport.deserializeUser((user, done) => {
-    done(null, user);
-  });
-  
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
 /* ***********************
  * Routes
- *************************/
+ * ***********************/
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-.use("/book", inventoryRoute)
-.use("/account", accountRoute)
-//new routes
-.use("/magazine", magazineRoute)
-.use("/digital", digitalRoute)
-.use("/", route);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/book", inventoryRoute);
+app.use("/account", accountRoute);
+app.use("/magazine", magazineRoute);
+app.use("/digital", digitalRoute);
+app.use("/", route);
 
 app.get("/", (req, res) => {
-  console.log(req.session.user)
-  
+  console.log(req.session.user);
   res.send(
     req.session.user !== undefined
       ? `Logged in as ${req.session.user.username}`
@@ -91,7 +86,7 @@ app.use(async (req, res, next) => {
   next({ status: 404, message: "Sorry, we appear to have lost that page." });
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Web service is listening on port: ${PORT}.`);
-});
+/* ***********************
+ * Export the app ONLY
+ * ***********************/
+module.exports = app;
